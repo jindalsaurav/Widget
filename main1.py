47d0984fc93d4171be006b0131765afbc9ed7cdc
Saurav Jindal <sauravjindal@outlook.com>
2026-05-04 23:06:27 +0530

    Up

--- main1.py
+++ main1.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Enquire Now", layout="centered")
 
@@ -24,4 +25,5 @@ st.markdown(
 
 form_page = Path(__file__).with_name("enquiry_form.html")
 
-st.iframe(form_page, height=760)
+#st.iframe(form_page, height=760)
+components.html(form_page.read_text(encoding="utf-8"), height=760, scrolling=True)
